ba899d036eedda8cfdab1a81c2c2f64119c707aa
allgiff <gif19002@byui.edu>
2022-12-01 11:18:59 -0700

    began final project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AuthorsComponent } from "./authors/authors.component";
+import { BooksComponent } from "./books/books.component";
+
+
+const appRoutes: Routes = [
+    { path: '', redirectTo: '/books', pathMatch: 'full'},
+    { path: 'books', component: BooksComponent},
+    { path: 'authors', component: AuthorsComponent},
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+    
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { HeaderComponent } from './header/header.component';
+import { BookListComponent } from './books/book-list/book-list.component';
+import { BookItemComponent } from './books/book-item/book-item.component';
+import { BookDetailComponent } from './books/book-detail/book-detail.component';
+import { BookEditComponent } from './books/book-edit/book-edit.component';
+import { AuthorListComponent } from './authors/author-list/author-list.component';
+import { AuthorItemComponent } from './authors/author-item/author-item.component';
+import { AuthorDetailComponent } from './authors/author-detail/author-detail.component';
+import { AuthorEditComponent } from './authors/author-edit/author-edit.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    AuthorsComponent,
+    HeaderComponent,
+    BookListComponent,
+    BookItemComponent,
+    BookDetailComponent,
+    BookEditComponent,
+    AuthorListComponent,
+    AuthorItemComponent,
+    AuthorDetailComponent,
+    AuthorEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
