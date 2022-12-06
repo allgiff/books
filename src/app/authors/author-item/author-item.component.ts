50ee94527e448538f7fd945d2bff61964ff08be7
allgiff <gif19002@byui.edu>
2022-12-05 23:11:28 -0700

    worked on getting author items to display

--- src/app/authors/author-item/author-item.component.ts
+++ src/app/authors/author-item/author-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Author } from '../author.model';
 
 @Component({
   selector: 'app-author-item',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./author-item.component.css']
 })
 export class AuthorItemComponent implements OnInit {
+  
+  @Input() author: Author;
+
+  authors: Author[] = [
+    new Author('', '2', 'Test Author', 'This is a Author', 'https://via.placeholder.com/150'),
+    new Author('', '2', 'Test Author', 'This is a Author', 'https://via.placeholder.com/150')
+  ];
 
   constructor() { }
 
